e63476af2cdd27aa2d50ad4d69c26866eecbb72a
unknown <https://github.com/ChristianBugwatAbamo/skillhub.git>
2024-12-09 20:22:49 +0800

    feat: updated applayout and views

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,72 +1,54 @@
 <script setup>
-import { ref } from 'vue'
-
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+import AppLayout from '@/components/layout/AppLayout.vue'
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3" color="blue-darken-4">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-16">
+          <v-card class="mx-auto" elavation="24">
+            <v-card-text class="text-center">
+              <v-img class="mx-auto rounded-circle" src="/images/logo.png" width="25%"></v-img>
+              <h1 class="font-weight-black">Skill Hub</h1>
+              <p class="font-weight-bold">Login Form</p>
+            </v-card-text>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="Firstname" variant="outlined"></v-text-field>
 
-      <v-main>
-        <v-container>
-          <v-row class="d-flex justify-center">
-            <v-col cols="12" md="6">
-              <v-card class="mx-auto" prepend-icon="mdi-account" subtitle="Login Form">
-                <template v-slot:title>
-                  <span class="font-weight-black">Skill Hub </span>
-                </template>
+                <v-text-field label="Lastname" variant="outlined"></v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field label="Firstname" variant="outlined"></v-text-field>
+                <v-text-field label="Email" variant="outlined"></v-text-field>
 
-                    <v-text-field label="Lastname" variant="outlined"></v-text-field>
+                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
 
-                    <v-text-field label="Email" variant="outlined"></v-text-field>
+                <v-text-field
+                  label="Password Confirmation"
+                  type="password"
+                  variant="outlined"
+                ></v-text-field>
 
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
+                <v-btn
+                  class="mt-2"
+                  type="submit"
+                  block
+                  color="primary"
+                  prepend-icon="mdi-account-plus"
+                  >Submit</v-btn
+                >
+              </v-form>
 
-                    <v-text-field
-                      label="Password Confirmation"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
+              <v-divider class="my-5"></v-divider>
 
-                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-                  </v-form>
-
-                  <v-divider class="my-5"></v-divider>
-
-                  <h5 class="text-center">
-                    Already have account? <RouterLink to="/">Click here to Login</RouterLink>
-                  </h5>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer color="blue-darken-4" border app>2024 - Skill Hub</v-footer>
-    </v-app>
-  </v-responsive>
+              <h5 class="text-center">
+                Already have account? <RouterLink to="/">Click here to Login</RouterLink>
+              </h5>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
