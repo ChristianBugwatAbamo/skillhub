a330f520e7574f46e6665fbaec469a074832d03a
unknown <https://github.com/ChristianBugwatAbamo/skillhub.git>
2024-12-09 18:40:06 +0800

    feat: added reisterview

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -33,6 +33,10 @@ function onClick() {
 
                 <v-card-text class="bg-surface-light pt-4">
                   <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Firstname" variant="outlined"></v-text-field>
+
+                    <v-text-field label="Lastname" variant="outlined"></v-text-field>
+
                     <v-text-field label="Email" variant="outlined"></v-text-field>
 
                     <v-text-field
@@ -41,14 +45,19 @@ function onClick() {
                       variant="outlined"
                     ></v-text-field>
 
+                    <v-text-field
+                      label="Password Confirmation"
+                      type="password"
+                      variant="outlined"
+                    ></v-text-field>
+
                     <v-btn class="mt-2" type="submit" block>Submit</v-btn>
                   </v-form>
 
-                  <v-divider class="my-5s"></v-divider>
+                  <v-divider class="my-5"></v-divider>
 
                   <h5 class="text-center">
-                    Don't have to account?
-                    <RouterLink to="/register">Click here to Register</RouterLink>
+                    Already have account? <RouterLink to="/">Click here to Login</RouterLink>
                   </h5>
                 </v-card-text>
               </v-card>
